5c4f1be146879f10bfcec94c7c42e5855fcc101f
Andrey Voloshin <153195067+ZfauX@users.noreply.github.com>
2025-01-17 13:57:01 +0300

    Update zm-install.sh

--- zoneminder/zm-install.sh
+++ zoneminder/zm-install.sh
@@ -14,12 +14,18 @@ apt update
 clear
 apt install zoneminder --install-recommends -y
 systemctl enable --now zoneminder
+openssl req -new -nodes -text -out root.csr -keyout root.key -subj "/C=RU/ST=Moskovskaya oblast'/L=Shahovskaya/O=OOO "ORION"/OU=IT/CN=root"
+chmod og-rwx root.key
+openssl x509 -req -in root.csr -text -days 365 -extfile /etc/ssl/openssl.cnf -extensions v3_ca -signkey root.key -out root.crt
 openssl req -new -nodes -text -out zoneminder.csr -keyout zoneminder.key -subj "/C=RU/ST=Moskovskaya oblast'/L=Shahovskaya/O=OOO "ORION"/OU=IT/CN=zm1.terminal.lft"
 chmod og-rwx zoneminder.key
-openssl x509 -req -in zoneminder.csr -text -days 365 -extfile /etc/ssl/openssl.cnf -extensions v3_ca -signkey zoneminder.key -out zoneminder.crt
-mv zoneminder.key /etc/ssl/private/ 
-mv zoneminder.crt /etc/ssl/certs/
-rm zoneminder.csr
+openssl x509 -req -in zoneminder.csr -text -days 365 -CA root.crt -CAkey root.key -CAcreateserial -out zoneminder.crt
+openssl req -new -nodes -text -out rtsptoweb.csr -keyout rtsptoweb.key -subj "/C=RU/ST=Moskovskaya oblast'/L=Shahovskaya/O=OOO "ORION"/OU=IT/CN=RTSPtoWeb"
+chmod og-rwx rtsptoweb.key
+openssl x509 -req -in rtsptoweb.csr -text -days 365 -CA root.crt -CAkey root.key -CAcreateserial -out rtsptoweb.crt
+mv zoneminder.key root.key rtsptoweb.key /etc/ssl/private/ 
+mv zoneminder.crt root.crt rtsptoweb.crt /etc/ssl/certs/
+rm zoneminder.csr rtsptoweb.csr root.csr
 adduser www-data video
 a2enconf zoneminder
 a2enmod rewrite
@@ -36,7 +42,11 @@ systemctl reload apache2
 cd $HOME
 git clone https://github.com/deepch/RTSPtoWeb
 cd $HOME/RTSPtoWeb
-sed -i 's/"http_demo": true/"http_demo": false/' config.json
+sed -i 's/"http_dir": "web"/"http_dir": "/root/RTSPtoWeb/web"/' config.json
+sed -i 's/"https": false/https": true/' config.json
+sed -i 's/"https_cert": "server.crt"/"https_cert": "/etc/ssl/certs/rtsptoweb.crt"/' config.json
+sed -i 's/"https_key": "server.key"/"https_key": "/etc/ssl/private/rtsptoweb.key"/' config.json
+sed -i 's/"https_port": ":443"/"https_port": ":8084"/' config.json
 go build
 mv $HOME/zoneminder/RTSPtoWeb/rtsptoweb.service /etc/systemd/system/
 systemctl daemon-reload
